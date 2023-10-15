f56c79edd0accbaf4235915f5ba6235a90ac34b4
Yousef. <kingyousef922@gmail.com>
2023-10-15 19:16:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => tutorial.split(" ").map(word => word[0].toUpperCase() + word.substring(1)).join(" "))
 }
+
+
+
